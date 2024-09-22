95c151b94f3dd002bb69c9814092b7f1f727f8c8
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2024-09-22 12:53:46 +0300

    Manual styling for market 1 to allow for custom HTML/CSS

--- frontend/src/features/MarketDetail/components/MarketDescription/index.tsx
+++ frontend/src/features/MarketDetail/components/MarketDescription/index.tsx
@@ -1,9 +1,10 @@
-import { Box, Typography } from "@mui/joy"
+import { Box, List, ListItem, Typography } from "@mui/joy"
 
 import type { Market } from "@api/queries/Market"
 import type { StyleProps } from "@utils/styles"
 import { LoadableWidget } from "@lib/Loadable/Widget"
 import { useSuspenseCurrentMarket } from "@features/MarketDetail/utils"
+import { CONTRACT_ADDRESS } from "@config/environment"
 
 const MarketDescription = (props: StyleProps) => {
   return (
@@ -25,8 +26,60 @@ const MarketDescriptionContent = (props: { market: Market }) => {
         Description
       </Typography>
 
-      <Typography level="body-md" whiteSpace="pre-line">
-        {market.description}
+      {CONTRACT_ADDRESS ===
+        "neutron14r4gqjhnuxzwcmypjle9grlksvjeyyy0mrw0965rdz5v3v5wcv6qjf8w55" &&
+      market.id === "1" ? (
+        <MainnetOneDescription />
+      ) : (
+        <Typography level="body-md" whiteSpace="pre-line">
+          {market.description}
+        </Typography>
+      )}
+    </>
+  )
+}
+
+const MainnetOneDescription = () => {
+  return (
+    <>
+      <Typography level="body-md">
+        This market tracks whether Donald Trump wins the presidency in the US
+        elections of 2024. This market will settle "Yes" if either of the
+        following two conditions occurs:
+      </Typography>
+      <List>
+        <ListItem>
+          <Typography level="body-md">
+            Donald Trump's primary opponent, Kamala Harris, concedes the
+            election to Donald Trump.
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <Typography level="body-md">
+            Donald Trump is inaugurated as president
+          </Typography>
+        </ListItem>
+      </List>
+      <Typography level="body-md">
+        By contrast, this market will settle "No" if either of the following two
+        conditions occurs:
+      </Typography>
+      <List>
+        <ListItem>
+          <Typography level="body-md">
+            Donald Trump concedes the election
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <Typography level="body-md">
+            Any other person is inaugurated as president
+          </Typography>
+        </ListItem>
+      </List>
+      <Typography level="body-md">
+        Note that, in the event of a contested election, this market may not
+        settle until January 2025, or if there are further uncertainties of the
+        outcome, may be further delayed.
       </Typography>
     </>
   )
