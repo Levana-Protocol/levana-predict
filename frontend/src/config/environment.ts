3f54a12dc8bfae69b920098cc15d86bdb44dce74
Alduin <alduin@levana.exchange>
2024-09-23 15:37:41 -0300

    Trigger build

--- frontend/src/config/environment.ts
+++ frontend/src/config/environment.ts
@@ -12,9 +12,11 @@ export const IS_TESTNET = ENV === "testnet"
 export const CONTRACT_ADDRESS = IS_TESTNET
   ? "neutron1juw8ztnzy37xs9nvcqf4lhshcg7zndwmjvgmzxkp8cgjgw3ahscshn2l82"
   : "neutron14r4gqjhnuxzwcmypjle9grlksvjeyyy0mrw0965rdz5v3v5wcv6qjf8w55"
+
 export const QUERIER_ADDRESS = IS_TESTNET
   ? "https://querier-testnet.levana.finance"
   : "https://querier-mainnet.levana.finance"
+
 export const INDEXER_ADDRESS = IS_TESTNET
   ? "https://indexer-testnet.levana.finance"
   : "https://indexer-mainnet.levana.finance"
